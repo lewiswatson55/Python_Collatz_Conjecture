6d778bac4894188727bfb21123525bf50179e920
Lewiswatson55 <Lewiswatson55@hotmail.co.uk>
2017-08-12 18:40:35 +0100

    Add files via upload

---
+++ Collatz_Conjecture.py
@@ -0,0 +1,25 @@
+#Collatz Conjecture Program
+#Created by Lewis Watson
+
+def Select_Number():
+    n = int(input("Select the number you wish to calculate? "))
+    return n
+
+def Calculate():
+    n = Select_Number();
+
+    while n != 1:
+        if (n % 2 == 0):
+            n = (n/2)
+            print (str(n) + " (Even)")
+        else:
+            n = (3*n+1)
+            print (str(n) + " (Odd)")
+
+    print ("The number has reached 1")
+    print()
+    Calculate();
+
+Calculate();
+
+
